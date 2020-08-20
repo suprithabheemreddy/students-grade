61169378e36d5b3a66380f751a99d66cc5e0527c
suprithabheemreddy <69784967+suprithabheemreddy@users.noreply.github.com>
2020-08-20 13:40:16 +0530

    Created students grade
    Students grade is created using if else if statement

---
+++ Students grade.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{
+int marks;
+printf("enter marks");
+scanf("%d",&marks);
+if(if marks are 85-100)
+{
+printf("grade A");
+}
+elseif(70-84)
+{
+printf("grade B");
+}
+elseif(marks are 55-69)
+{
+printf("grade C");
+}
+elseif(marks are 40-54)
+{
+printf("grade D")
+}
+else(marks are below 40)
+{
+printf("grade F")
+}
+}
